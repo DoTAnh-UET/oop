834a7a2c06b766aac96ed2e0797cb5bc6ba4be89
DoTAnh-UET <24021362@vnu.edu.vn>
2025-09-24 21:42:23 +0700

    Phan C HW1 1.2.18 1.2.20 1.2.23 1.3.1 1.3.6

---
+++ SpringSeason.java
@@ -0,0 +1,11 @@
+public class SpringSeason {
+    public static void main(String[] args) {
+        int day = Integer.parseInt(args[0]);
+        int month = Integer.parseInt(args[1]);
+        boolean isSpringSeason = (month == 3 && day >= 20 && day <= 31) || (month == 4 && day >= 1 && day <= 30) ||
+                (month == 5 && day >= 1 && day <= 31 ) || (month == 6 && day >= 1 && day <= 20 );
+        System.out.println(isSpringSeason);
+
+    }
+
+}
