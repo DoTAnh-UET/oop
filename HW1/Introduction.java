e82dafc7bacd8f12deb9691e3d9e1afda3dcc458
DoTAnh-UET <24021362@vnu.edu.vn>
2025-09-24 15:35:52 +0700

    Phan B HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("Do Tuan Anh\t" + "24021362\t" + "K69-QH-I-2024-CS5\t" + "DoTAnh-UET\t" + "24021362@vnu.edu.vn");
+        for(int i = 9; i > 0; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
